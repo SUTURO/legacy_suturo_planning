109b6cce2f67f44b333c82b3faddf7a9e84a72a3
Tom Gehrke <tgehrke@tzi.de>
2013-11-11 14:01:46 +0100

    Define top level plan touch-edible

--- suturo_executive/src/suturo-executive.lisp
+++ suturo_executive/src/suturo-executive.lisp
@@ -1,1 +1,5 @@
 (in-package :suturo-executive)
+
+(def-top-level-cram-function touch-edible ()
+  "Finds and touches the edible object out of three objects located on the table"
+  (ros-info (suturo-executive) "TODO"))
