d2753918cb1d0f6553cdc57a73f5a3ab9b0d9ad1
Jannik Buckelo <jannikbu@tzi.de>
2013-11-18 13:14:14 +0100

    PLAN-10 #time 1h #comment top-level-plan without failure handling

--- suturo_executive/src/suturo-executive.lisp
+++ suturo_executive/src/suturo-executive.lisp
@@ -1,8 +1,17 @@
 (in-package :suturo-executive)
 
+;(defmacro with-process-modules (&body body)
+;  `(cpm:with-process-modules-running
+;       (suturo-process-module:suturo-process-module)
+;     ,@body))
+
 (def-top-level-cram-function touch-edible ()
-  "Finds and touches the edible object out of three objects located on the table"
-  (ros-info (suturo-executive) "TODO"))
+  "Finds and touches the edible object out of the objects located on the table"
+  (suturo-planlib::reach-position 'suturo-planlib:initial)
+  (let (perceived-objects `(suturo-planlib::find-objects))
+    (let (filtered-objects `(suturo-planlib::filter-objects perceived-objects))
+      (suturo-planlib::touch-object (first filtered-objects))))
+  (ros-info (suturo-executive) "PLAN SUCCESS"))
 
 (def-top-level-cram-function test-planlib ()
   (suturo-planlib::test-plan 'suturo-planlib:test1))
