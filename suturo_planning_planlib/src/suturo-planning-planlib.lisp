0cb6f42826ed3a78fe2a42980ce16bc3f475d815
Hannes Welling <hannes.welling@uni-bremen.de>
2013-11-23 16:32:30 +0100

    PLAN-14 #time 30m #comment Dokumentation in 'suturo-planning-planlib.lisp' erstellt

--- suturo_planning_planlib/src/suturo-planning-planlib.lisp
+++ suturo_planning_planlib/src/suturo-planning-planlib.lisp
@@ -1,38 +1,47 @@
 (in-package :suturo-planning-planlib)
 
 (define-condition food-overflow (simple-plan-failure)
+  "Condition is thrown if to many edible objects found."
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "food-overflow"))
 
 (define-condition no-food-found (simple-plan-failure)
+  "Condition is thrown if no edible objects found."
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "no-food-found"))
 
 (define-condition touch-failed (simple-plan-failure)
+  "Condition is thrown if the robot failed to touch the edible object."
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "touch-failed"))
 
 (define-condition pose-not-reached (simple-plan-failure)
+  "Condition is thrown if the selected pose could not be reached by the robot."
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "pose-not-reached"))
 
 (declare-goal test-plan (indicator)
+  "Testrun for top-level plan."
   (roslisp:ros-info (suturo planlib)
                     "TEST-PLAN ~a" indicator))
 
 (declare-goal reach-position (indicator)
+  "Manipulation moves arms into position"
   (roslisp:ros-info (suturo planlib)
                     "INITIAL-POSITION ~a" indicator))
 
 (declare-goal find-objects ()
+  "Perception perceives objects"
   (roslisp:ros-info (suturo planlib)
                     "FIND-OBJECTS"))
 
 (declare-goal get-edible-objects (indicator designators)
+  "Knowledge Representation returns edible object(s)"
   (roslisp:ros-info (suturo planlib)
                     "GET-EDIBLE-OBJECTS ~a ~a" indicator designators))
 
 (declare-goal touch-object (indicator designator)
+  "Manipulation moves arm to object"
   (roslisp:ros-info (suturo planlib)
                     "TOUCH-OBJECT ~a" designator))              
 
@@ -75,8 +84,10 @@
       (cond ((= (length edible-objects) 1)
              edible-objects)
             ((= (length edible-objects) 0)
+             ;Condtion is thrown if no edible objects were found.
              (cpl:error 'no-food-found
                         :result edible-objects))
+             ;Condition is thrown if to many (>1) edible objects ware found.
             (t (cpl:error 'food-overflow
                           :result edible-objects)))
       (roslisp:ros-info (suturo planlib)
