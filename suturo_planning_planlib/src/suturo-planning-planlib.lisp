f4c49d65615da79abb24630e87e65fdd2ffcb503
Hannes Welling <hwelling@tzi.de>
2013-11-23 17:59:41 +0100

    PLAN-14 #time 20m #comment Fehlerhafte Dokumentation korriegiert, ';' statt ''

--- suturo_planning_planlib/src/suturo-planning-planlib.lisp
+++ suturo_planning_planlib/src/suturo-planning-planlib.lisp
@@ -1,47 +1,47 @@
 (in-package :suturo-planning-planlib)
 
 (define-condition food-overflow (simple-plan-failure)
-  "Condition is thrown if to many edible objects found."
+  ;Condition is thrown if to many edible objects found.
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "food-overflow"))
 
 (define-condition no-food-found (simple-plan-failure)
-  "Condition is thrown if no edible objects found."
+  ;Condition is thrown if no edible objects found.
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "no-food-found"))
 
 (define-condition touch-failed (simple-plan-failure)
-  "Condition is thrown if the robot failed to touch the edible object."
+  ;Condition is thrown if the robot failed to touch the edible object.
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "touch-failed"))
 
 (define-condition pose-not-reached (simple-plan-failure)
-  "Condition is thrown if the selected pose could not be reached by the robot."
+  ;Condition is thrown if the selected pose could not be reached by the robot.
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "pose-not-reached"))
 
 (declare-goal test-plan (indicator)
-  "Testrun for top-level plan."
+  ;Testrun for top-level plan.
   (roslisp:ros-info (suturo planlib)
                     "TEST-PLAN ~a" indicator))
 
 (declare-goal reach-position (indicator)
-  "Manipulation moves arms into position"
+  ;Manipulation moves arms into position
   (roslisp:ros-info (suturo planlib)
                     "INITIAL-POSITION ~a" indicator))
 
 (declare-goal find-objects ()
-  "Perception perceives objects"
+  ;Perception perceives objects
   (roslisp:ros-info (suturo planlib)
                     "FIND-OBJECTS"))
 
 (declare-goal get-edible-objects (indicator designators)
-  "Knowledge Representation returns edible object(s)"
+  ;Knowledge Representation returns edible object(s)
   (roslisp:ros-info (suturo planlib)
                     "GET-EDIBLE-OBJECTS ~a ~a" indicator designators))
 
 (declare-goal touch-object (indicator designator)
-  "Manipulation moves arm to object"
+  ;Manipulation moves arm to object
   (roslisp:ros-info (suturo planlib)
                     "TOUCH-OBJECT ~a" designator))              
 
