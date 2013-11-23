f2fa21b290c074980d3f42874517c927624bbd53
andz <aromero@informatik.uni-bremen.de>
2013-11-23 13:59:59 +0100

    Formatierung

--- suturo_planning_planlib/src/package.lisp
+++ suturo_planning_planlib/src/package.lisp
@@ -1,9 +1,14 @@
 (in-package :cl-user)
 
 (desig-props:def-desig-package suturo-planning-planlib
-  (:use #:roslisp #:cram-utilities #:designators-ros
-        #:cram-roslisp-common #:cram-designators
-        #:cram-plan-knowledge #:cram-plan-library #:cpl
+  (:use #:roslisp
+        #:cram-utilities
+        #:designators-ros
+        #:cram-roslisp-common
+        #:cram-designators
+        #:cram-plan-knowledge
+        #:cram-plan-library
+        #:cpl
     	  #:cram-plan-failures)
   (:import-from #:cram-reasoning #:<- #:def-fact-group)
   (:export test1 initial a the all right left)
