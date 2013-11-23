0f2ec0101f6a15edd4305b128151233dc1216565
Hannes Welling <hwelling@tzi.de>
2013-11-23 17:59:41 +0100

    PLAN-14 #time 20m #comment Fehlerhafte Dokumentation korriegiert, ';' statt ''

--- suturo_planning_planlib/src/suturo-planning-planlib.lisp
+++ suturo_planning_planlib/src/suturo-planning-planlib.lisp
@@ -1,22 +1,22 @@
 (in-package :suturo-planning-planlib)
 
 (declare-goal reach-position (indicator)
-  "Manipulation moves arms into position"
+  ;Manipulation moves arms into position
   (roslisp:ros-info (suturo planlib)
                     "INITIAL-POSITION ~a" indicator))
 
 (declare-goal find-objects ()
-  "Perception perceives objects"
+  ;Perception perceives objects
   (roslisp:ros-info (suturo planlib)
                     "FIND-OBJECTS"))
 
 (declare-goal get-edible-objects (indicator designators)
-  "Knowledge Representation returns edible object(s)"
+  ;Knowledge Representation returns edible object(s)
   (roslisp:ros-info (suturo planlib)
                     "GET-EDIBLE-OBJECTS ~a ~a" indicator designators))
 
 (declare-goal touch-object (indicator designator)
-  "Manipulation moves arm to object"
+  ;Manipulation moves arm to object
   (roslisp:ros-info (suturo planlib)
                     "TOUCH-OBJECT ~a" designator))              
 
