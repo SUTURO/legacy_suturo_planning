ec43f81f591930c4b7a72ec9572f737501c41a9d
Tom Gehrke <tgehrke@tzi.de>
2013-11-24 11:15:49 +0100

    Commented utility functions

--- suturo_planning_process_module/src/util.lisp
+++ suturo_planning_process_module/src/util.lisp
@@ -1,6 +1,8 @@
 (in-package :suturo-planning-process-module)
 
 (defun parse-perceived-objects-to-string (objs)
+  "Parses a list of objects of type PerceivedObject to string
+  for the use with a json_prolog service"
   (let ((result "["))
     (loop for i from 0 below (length objs)
           for obj = (elt objs i)
@@ -12,6 +14,8 @@
     (concatenate 'string (subseq result 0 (- (length result) 2)) "]")))
 
 (defun parse-perceived-object-to-string (obj)
+  "Parses the given object of type PerceivedObject to string
+  for the use with a json_prolog service"
 ; uint32 (float64 float64 float64) float32
   (roslisp:with-fields (c_id c_volume recognition_label_2d shape) obj 
     (concatenate 'string 
@@ -28,12 +32,14 @@
                  "]")))
 
 (defun parse-object-ids-from-string (str)
+  "Parses a comma seperated list of numbers to a lisp-list of numbers"
   (let ((result '()))
     (ppcre:do-matches (s e "[0-9]{1,}" str nil :start 0 :end (length str))
                       (push (read-from-string (subseq str s e)) result))
     result))
 
 (defun filter-objects-by-ids (objs ids)
+  "Filters a list of objects of type PerceivedObject by a list of given ids"
   (let ((objs-to-delete nil))
     (loop for obj across objs
           do
@@ -45,5 +51,3 @@
     (loop for obj in objs-to-delete
           do
           (remove obj objs))))
-
-             
