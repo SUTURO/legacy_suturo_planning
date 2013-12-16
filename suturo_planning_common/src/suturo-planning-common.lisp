3e30df4042c7312e640328bd6eb5ec5bc05bdbe1
Tom Gehrke <tgehrke@tzi.de>
2013-12-16 14:09:11 +0100

    PLAN-43 #time 3h Created designator->string function for the communication with the knowledge base

--- suturo_planning_common/src/suturo-planning-common.lisp
+++ suturo_planning_common/src/suturo-planning-common.lisp
@@ -19,3 +19,24 @@
 (define-condition no-object-perceived (simple-plan-failure)
   ((result :initarg :result :reader result :initform nil))
   (:default-initargs :format-control "no-object-perceived"))
+
+(defun designator->string (desig)
+  "Returns a string containing the properties of the given designator"
+  (let ((des (description desig)))
+    (list->string des)))
+
+(defun element->string (e)
+  "Returns a string representation of the given parameter. If a list is given, the list is converted to a string."
+  (if (listp e)
+    (list->string e)
+    (write-to-string e)))
+
+(defun list->string (l)
+  "Converts a given list to a string with a format of [a,b,c,...]"
+  (let ((result ""))
+    (dolist (e l)
+      (setq result (concatenate 'string result 
+                                (element->string e) ",")))
+    (concatenate 'string "[" 
+                 (subseq result 0 (- (length result) 1)) 
+                 "]")))
