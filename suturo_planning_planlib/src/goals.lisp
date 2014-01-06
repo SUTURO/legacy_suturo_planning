b71a1add4896d991261e01740c008798375f7565
Jannik Buckelo <jannikbu@tzi.de>
2014-01-06 12:00:42 +0100

    PLAN-16 #time 10m added 'desig-props:'

--- suturo_planning_planlib/src/goals.lisp
+++ suturo_planning_planlib/src/goals.lisp
@@ -2,8 +2,8 @@
 
 (def-goal (achieve (home-pose))
   (with-designators ((take-home-pose (action 
-                                      '((to take-pose)
-                                        (pose home)))))
+                                      '((desig-props:to take-pose)
+                                        (desig-props:pose home)))))
     (perform take-home-pose)
     (format t "Initial pose reached")))
 
@@ -12,16 +12,16 @@
   (let ((arm (get-best-arm ?obj))
         (fails 0))
     (with-failure-handling 
-        ((grasping-failed (f)
+        ((simple-plan-failure (f)
            (declare (ignore f))
            (incf fails)
            (if (< fails 4)
                (seq
                  (setf arm (switch-arms arm))
                  (retry)))))
-      (with-designators ((grasp-obj (action `((to grasp)
-                                              (obj ,?obj)
-                                              (arm ,arm)))))
+      (with-designators ((grasp-obj (action `((desig-props:to grasp)
+                                              (desig-props:obj ,?obj)
+                                              (desig-props:arm ,arm)))))
         (perform grasp-obj))))
   (format t "~a in hand" ?obj))
 
@@ -31,17 +31,17 @@
     (setf coords `(,(nth 0 coords)
                    ,(nth 1 coords)
                    ,(+ (nth 2 coords) 10)))
-    (with-designators ((loc-over-obj (location `((coords ,coords)))))
-      (with-designators ((move-hand (action `((to move-arm)
-                                              (arm ,?arm)
-                                              (loc ,loc-over-obj)))))
+    (with-designators ((loc-over-obj (location `((desig-props:coords ,coords)))))
+      (with-designators ((move-hand (action `((desig-props:to move-arm)
+                                              (desig-props:arm ,?arm)
+                                              (desig-props:loc ,loc-over-obj)))))
         (perform move-hand))))
   (format t "Hand over ~a" ?obj))
 
 (def-goal (achieve (empty-hand ?arm))
   "Opens the hand of the given arm"
-  (with-designators ((open-hand (action `((to open-hand)
-                                          (arm ,?arm)))))
+  (with-designators ((open-hand (action `((desig-props:to open-hand)
+                                          (desig-props:arm ,?arm)))))
     (perform open-hand))
   (format t "Hand empty"))
 
@@ -71,7 +71,7 @@
                  (retry)))))
       (loop while ?objs
             do (setf obj (pop ?objs))
-               (if (desig-prop-value obj 'edible)
+               (if (desig-prop-value obj 'desig-props:edible)
                    (setf box left-box)
                    (setf box right-box))
                (achieve `(object-in-box ,obj ,box)))))
@@ -82,11 +82,14 @@
         (boxes nil))
     (loop while (and (< (length objs) ?nr-objs) (< (length boxes) ?nr-boxes))
           do (with-designators ((update-semantic-map 
-                                (action '((to update-sematic-map))))
+                                (action 
+                                 '((desig-props:to update-sematic-map))))
                                (get-containers 
-                                (action '((to get-container-objects))))
+                                (action 
+                                 '((desig-props:to get-container-objects))))
                                (get-objects
-                                (action '((to get-graspable-objects)))))
+                                (action 
+                                 '((desig-props:to get-graspable-objects)))))
                (perform update-semantic-map)
                (setf objs (perform get-objects))
                (setf boxes (perform get-containers))))
@@ -94,8 +97,8 @@
 
 (defun get-box (boxes side)
   "Returns the box that is on the given side of the other box"
-  (let ((x1 (first (desig-prop-value (first boxes) 'coords)))
-        (x2 (first (desig-prop-value (second boxes) 'coords))))
+  (let ((x1 (first (desig-prop-value (first boxes) 'desig-props:coords)))
+        (x2 (first (desig-prop-value (second boxes) 'desig-props:coords))))
     (if (eql side 'left)
         (if (< x1 x2)
             (first boxes)
@@ -107,23 +110,28 @@
 (defun get-holding-hand (obj)
   "Returns the arm which holds the object"
   (let ((pos (desig-prop-value 
-              (desig-prop-value (current-desig obj) 'at) 
-              'in)))
+              (desig-prop-value (current-desig obj) 'desig-props:at) 
+              'desig-props:in)))
     (if (not pos)
         (format t "ERROR2")
         (seq 
-          (if (eql pos 'left-gripper) 
+          (if (eql pos 'desig-props:left-gripper) 
               'left-arm
-              (if (eql pos 'right-gripper)
+              (if (eql pos 'desig-props:right-gripper)
                   'right-arm))))))
 
 (defun get-best-arm (obj)
   "Returns the arm closest to the object"
   (let ((coords (get-coords obj)))
     (if (< (first coords) 0)
-        'left-arm
-        'right-arm)))
+        'desig-props:left-arm
+        'desig-props:right-arm)))
 
 (defun get-coords (obj)
   "Returns the coordinates of the object"
-  (desig-prop-value (desig-prop-value obj 'at) 'coords))
+  (desig-prop-value (desig-prop-value obj 'desig-props:at) 'desig-props:coords))
+
+(defun switch-arms (arm)
+  (if (eql 'desig-props:left-arm arm)
+      'desig-props:left-arm
+      'desig-props:right-arm))
