dc9421953f12a365db6f8165a3a4ad72ea24e5f3
andz <aromero@informatik.uni-bremen.de>
2014-01-11 15:05:17 +0100

    PLAN-21 #time 3h 30m Edited pm for kr.

--- suturo_planning_pm_knowledge/src/action-handlers.lisp
+++ suturo_planning_pm_knowledge/src/action-handlers.lisp
@@ -7,31 +7,30 @@
 
 (def-action-handler update-semantic-map ()
   "Invokes Knowledge Representation's process to update the semantic map."
-  (let ((query (concatenate 'string "update_semantic_map(Answer)"))
-        (service "/json_prolog/simple_query")
-        (ret-service "/json_prolog/next_solution")
-        (fin-service "/json_prolog/finish"))
-    (format t "~a~%" query)
-    (if (not (roslisp:wait-for-service service 10))
-        (roslisp:ros-warn nil "Timed out waiting for simple_query")
-        (let ((result (roslisp:call-service
-                       service
-                       "json_prolog/PrologQuery"
-                       :mode 0
-                       :id ""
-                       :query query)))
-          (roslisp:with-fields (ok message) result
-            (format t "~a~%" ok)
-            (format t "~a~%" message)
-            (if ok
-                (roslisp:ros-info nil "Successfully updated semantic map.")
-                (roslisp:ros-warn nil "Could not update semantic map.")))))))
-                
+  (let ((gen (json-prolog:prolog-simple-1 "edibleOnTable(table, OUT)")))
+        (if gen
+            (with-vars-bound
+                (?OUT) gen
+              (format t "~a~%" ?OUT)
+              ?OUT)
+            (roslisp:ros-warn nil "Could not update semantic map."))))
 
 (def-action-handler get-container-objects ()
   "Receives all containers from Knowledge Representation as a list."
-  (nil))
+    (let ((gen (json-prolog:prolog-simple-1 "edibleOnTable(table, OUT)")))
+        (if gen
+            (with-vars-bound
+                (?OUT) gen
+              (format t "~a~%" ?OUT)
+              ?OUT)
+            (roslisp:ros-warn nil "Could not receive container objects."))))
 
 (def-action-handler get-graspable-objects ()
   "Receives all graspable objects from Knowledge Representation as a list"
-  (nil))
+   (let ((gen (json-prolog:prolog-simple-1 "containerOnTable(table, OUT)")))
+        (if gen
+            (with-vars-bound
+                (?OUT) gen
+              (format t "~a~%" ?OUT)
+              ?OUT)
+            (roslisp:ros-warn nil "Could not receive graspable objects."))))
