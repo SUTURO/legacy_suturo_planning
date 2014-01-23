5ce114ef7673932b007c215d63187a8b461effa9
Tom Gehrke <tgehrke@tzi.de>
2014-01-23 14:04:05 +0100

    Renamed top level plan

--- suturo_planning_executive/src/suturo-planning-executive.lisp
+++ suturo_planning_executive/src/suturo-planning-executive.lisp
@@ -23,31 +23,25 @@
     (clean-table-plan)))
 
 (def-cram-function clean-table-plan ()
-  (format t "This is a plan"))
-
-
-
-(def-top-level-cram-function sort-objs ()
-  (with-dummy-process-modules
-    (with-failure-handling
-      ((suturo-planning-common::pose-not-reached (f)
-        (declare (ignore f))
-        (error-out (planning exec) "Could not reach initial pose.")))
-      (suturo-planning-planlib::achieve 
-        '(suturo-planning-planlib::home-pose)))
-    (with-failure-handling
-      ((suturo-planning-common::no-object-perceived (f)
-        (declare (ignore f))
-        (error-out (planning exec) "No more objects found.")))
-      (let ((result (suturo-planning-planlib::achieve
-                      '(suturo-planning-planlib::objects-and-boxes-perceived 3 2))))
-        (with-failure-handling
-          ((suturo-planning-common::grasping-failed (f)
-            (declare (ignore f))
-            (error-out (planning exec) "Grasping failed.")))
-          (suturo-planning-planlib::achieve 
-            `(suturo-planning-planlib::objects-in-appropriate-boxes ,(first result)
-                                                                    ,(second result))))))))
+  (with-failure-handling
+    ((suturo-planning-common::pose-not-reached (f)
+      (declare (ignore f))
+      (error-out (planning exec) "Could not reach initial pose.")))
+    (suturo-planning-planlib::achieve 
+      '(suturo-planning-planlib::home-pose)))
+  (with-failure-handling
+    ((suturo-planning-common::no-object-perceived (f)
+      (declare (ignore f))
+      (error-out (planning exec) "No more objects found.")))
+    (let ((result (suturo-planning-planlib::achieve
+                    '(suturo-planning-planlib::objects-and-boxes-perceived 3 2))))
+      (with-failure-handling
+        ((suturo-planning-common::grasping-failed (f)
+          (declare (ignore f))
+          (error-out (planning exec) "Grasping failed.")))
+        (suturo-planning-planlib::achieve 
+          `(suturo-planning-planlib::objects-in-appropriate-boxes ,(first result)
+                                                                  ,(second result)))))))
 
 
 ;;; "test"
