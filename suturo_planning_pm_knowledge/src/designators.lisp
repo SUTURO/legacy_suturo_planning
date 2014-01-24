5ab9ec338b717ac53e5efffed10fdc6a24b98bdf
Jannik Buckelo <jannikbu@tzi.de>
2014-01-24 23:01:49 +0100

    fixed formating

--- suturo_planning_pm_knowledge/src/designators.lisp
+++ suturo_planning_pm_knowledge/src/designators.lisp
@@ -3,25 +3,25 @@
 (def-fact-group suturo-knowledge-designators (action-desig)
   
   (<- (action-desig ?desig (update-semantic-map))
-      ;;Action designator for action to get information about objects.
-      (desig-prop ?desig (to update-semantic-map)))
+    ;;Action designator for action to get information about objects.
+    (desig-prop ?desig (to update-semantic-map)))
 
   (<- (action-desig ?desig (placed-object-in-box ?obj ?container))
-      (desig-prop ?desig (to placed-object-in-box))
-      (desig-prop ?desig (obj ?obj))
-      (desig-prop ?desig (container ?container)))
-
+    (desig-prop ?desig (to placed-object-in-box))
+    (desig-prop ?desig (obj ?obj))
+    (desig-prop ?desig (container ?container)))
+  
   (<- (action-desig ?desig (get-container-objects))
-      ;;Action designator for action to get information about objects.
-      (desig-prop ?desig (to get-container-objects)))
-
+    ;;Action designator for action to get information about objects.
+    (desig-prop ?desig (to get-container-objects)))
+  
   (<- (action-desig ?desig (get-graspable-objects))
-      ;;Action designator for action to get information about objects.
-      (desig-prop ?desig (to get-graspable-objects))))
+    ;;Action designator for action to get information about objects.
+    (desig-prop ?desig (to get-graspable-objects))))
 
 (def-fact-group suturo-planning-pm-knowledge (matching-process-module
                                               available-process-module)
-
+  
   (<- (matching-process-module ?designator suturo-planning-pm-knowledge)
     (or (desig-prop ?designator (to update-semantic-map))
         (desig-prop ?designator (to placed-object-in-box))
