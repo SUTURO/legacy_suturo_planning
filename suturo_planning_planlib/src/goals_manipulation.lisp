1146c90bef097d1c09982bd56b621a5022754244
andz <aromero@informatik.uni-bremen.de>
2014-02-21 12:28:25 +0100

    PLAN-66 #time 15m

---
+++ suturo_planning_planlib/src/goals_manipulation.lisp
@@ -0,0 +1,22 @@
+(in-package :suturo-planning-planlib)
+
+(def-goal (achieve (?obj placed ?loc))
+  "Places an object `?obj' on a given location `loc'.
+The location has to be reachable without having to move the robot's base."
+  (info-out (suturo planlib) "Placing object gently.")
+  (sleep 1.0)
+  (with-retry-counters ((place-retry-counter 2))
+    (let* ((arm (get-holding-arm ?obj)))
+      (with-failure-handling
+          ((suturo-planning-common::place-failed (f)
+             (declare (ignore f))
+             (error-out (suturo planlib) "Failed to place object gently on given location.")
+             (sleep 2.0)
+             (do-retry place-retry-counter
+               (info-out (suturo planib) "Trying again.")
+               (sleep 1.0)
+               (retry))))
+        (with-designators ((open-hand (action `((to open-hand)
+                                                (obj ,?obj)))))
+          (perform open-hand)))))
+  (info-out (suturo planlib) "Droped, object"))
