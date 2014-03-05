ef651f7190aeb9eca4f99f26f4e59a4a3922c950
Tom Gehrke <tgehrke@tzi.de>
2014-03-05 18:45:08 +0100

    Move base to location to see objects

--- suturo_planning_planlib/src/goals-knowledge.lisp
+++ suturo_planning_planlib/src/goals-knowledge.lisp
@@ -10,8 +10,18 @@
           (let* ((on-obj (perceive (make-designator 'object `((name ,on)))))
                  (over-obj (perform (make-designator 'action `((to get-location-over) (loc ,loc))))))
             ; TODO: Bewegen
+            (perform (make-designator 'action `((to move-base) (pose ,(reference (make-designator 'location `((to see) (name "kitchen_island"))))))))
             (perform (make-designator 'action `((to move-head) (loc ,over-obj))))
             (perform (make-designator 'action '((to update-semantic-map))))
             (perceive ?obj))))
       objs)))
-
+; get objs from knwoledge
+; if not objs
+;   get 'on from description -> o
+;   get o from knowledge
+;   if o
+;     drive to coords(o) + some offset
+;     update semantic map
+;     get objs from knowledge
+;   else fail
+;     
